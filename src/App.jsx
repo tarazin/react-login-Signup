6559aa0f3f91f4cc61e204a9bab454e03e08ef6d
Zahra Azin <ztara.azin@gmail.com>
2020-11-06 14:20:32 -0500

    details added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from 'react-responsive';
 import "./App.scss";
 import { Login, Register } from "./components/login/index";
 
